122a3e4d189aa2df1dcd51d1e7941877fe667619
wildone <wildone@users.noreply.github.com>
2020-06-17 14:23:00 +0000

    Automated deployment: Wed Jun 17 14:23:00 UTC 2020 c1ceef70305e8559ceb3c0bb31aaff269f4c2f1a

--- assets/js/lunr/lunr-en.js
+++ assets/js/lunr/lunr-en.js
@@ -1,4 +1,348 @@
-var idx = lunr(function () {
+<!doctype html>
+<html lang="en" class="no-js">
+  <head>
+    <meta charset="utf-8">
+
+<!-- begin SEO -->
+
+
+
+
+
+
+
+
+
+<title>Max Barrass</title>
+
+
+
+
+<meta name="description" content="var idx = lunr(function () {  this.field(‘title’)  this.field(‘excerpt’)  this.field(‘categories’)  this.field(‘tags’)  this.ref(‘id’)">
+
+
+
+
+<meta property="og:locale" content="en">
+<meta property="og:site_name" content="Max Barrass">
+<meta property="og:title" content="Max Barrass">
+
+
+  <link rel="canonical" href="https://maxbarrass.com/assets/js/lunr/lunr-en.js">
+  <meta property="og:url" content="https://maxbarrass.com/assets/js/lunr/lunr-en.js">
+
+
+
+  <meta property="og:description" content="var idx = lunr(function () {  this.field(‘title’)  this.field(‘excerpt’)  this.field(‘categories’)  this.field(‘tags’)  this.ref(‘id’)">
+
+
+
+
+
+  
+
+  
+
+
+
+  <meta property="og:image" content="/assets/images/site-logo.png">
+
+
+
+
+
+
+
+
+  <script type="application/ld+json">
+    {
+      "@context": "http://schema.org",
+      "@type": "Organization",
+      "url": "https://maxbarrass.com",
+      "logo": "/assets/images/site-logo.png"
+    }
+  </script>
+
+
+
+  <script type="application/ld+json">
+    {
+      "@context" : "http://schema.org",
+      "@type" : "Person",
+      "name" : "Max Barrass",
+      "url" : "https://maxbarrass.com",
+      "sameAs" : null
+    }
+  </script>
+
+
+
+  <meta name="google-site-verification" content="UuKsKonXwFAcpjarBr3SIx6p8QGmuP_VHN8uMUfIgBw" />
+
+
+
+
+<!-- end SEO -->
+
+
+
+
+<meta name="author" content="">
+
+<link href="/feed.xml" type="application/atom+xml" rel="alternate" title="Max Barrass Feed">
+
+<!-- http://t.co/dKP3o1e -->
+<meta name="HandheldFriendly" content="True">
+<meta name="MobileOptimized" content="320">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<script>
+  document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/g, '') + ' js ';
+</script>
+
+<!-- For all browsers -->
+<link rel="stylesheet" href="/assets/css/main.css">
+
+<meta http-equiv="cleartype" content="on">
+
+
+<script>
+    function wrap(el, wrapper) {
+        el.parentNode.insertBefore(wrapper, el);
+        wrapper.appendChild(el);
+    }
+
+    document.addEventListener("DOMContentLoaded", function() {
+        var elements = document.querySelectorAll(".page__content p img");
+        elements.forEach(element => {
+            var url = element.getAttribute('src');
+            var alt = element.getAttribute('alt');
+            if(url) {
+                var a = document.createElement('a');
+                a.setAttribute("href",url);
+                a.setAttribute("data-lightbox",'');
+                a.setAttribute("title",alt);
+                wrap(element, a);
+            }
+        });
+
+    });
+</script>
+
+    <!-- start custom head snippets -->
+
+<!-- insert favicons. use http://realfavicongenerator.net/ -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css" rel="stylesheet">
+
+<link rel="apple-touch-icon" sizes="57x57" href="/assets/images/icons/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/assets/images/icons/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/assets/images/icons/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/assets/images/icons/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/assets/images/icons/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/assets/images/icons/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/assets/images/icons/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/assets/images/icons/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/assets/images/icons/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="/assets/images/icons/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/assets/images/icons/android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="/assets/images/icons/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="/assets/images/icons/manifest.json">
+<link rel="mask-icon" href="/assets/images/icons/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-TileImage" content="/assets/images/icons/mstile-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
+
+
+<style>
+    .archive__item-teaser {
+        min-height: 238px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: rgb(222,228,228);
+        background: radial-gradient(circle, rgba(222,228,228,1) 0%, rgba(255,179,107,1) 100%);
+    }
+
+    /*add tint to images*/
+    .page__hero--overlay {
+        background-color: rgba(255,179,107,.5);
+        background-blend-mode: multiply;
+    }
+</style>
+
+<!-- end custom head snippets -->
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-9B95B');</script>
+<!-- End Google Tag Manager -->
+  </head>
+
+  <body class="layout--single">
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-9B95B"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+    <!--[if lt IE 9]>
+<div class="notice--danger align-center" style="margin: 0;">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience.</div>
+<![endif]-->
+
+
+    
+
+<div class="masthead">
+    <div class="masthead__inner-wrap">
+        <div class="masthead__menu">
+            <nav id="site-nav" class="greedy-nav">
+                
+                <a class="site-title" href="/">
+                    Max Barrass
+                    
+                </a>
+                <ul class="visible-links"><li class="masthead__menu-item">
+                        <a target="" href="/">Home</a>
+                    </li><li class="masthead__menu-item">
+                        <a target="" href="/tools/">Tools</a>
+                    </li><li class="masthead__menu-item">
+                        <a target="" href="/blog/">Blog</a>
+                    </li><li class="masthead__menu-item">
+                        <a target="" href="/about/">About</a>
+                    </li><li class="masthead__menu-item">
+                        <a target="_blank" href="https://github.com/wildone/maxbarrass.com">GitHub</a>
+                    </li><li class="masthead__menu-item">
+                        <a target="" href="/search/">Search</a>
+                    </li></ul>
+                
+                <button class="greedy-nav__toggle hidden" type="button">
+                    <span class="visually-hidden">Toggle Menu</span>
+                    <div class="navicon"></div>
+                </button>
+                <ul class="hidden-links hidden"></ul>
+            </nav>
+        </div>
+    </div>
+</div>
+
+    
+
+
+
+<div id="main" role="main">
+    
+  <div class="sidebar sticky">
+    
+
+<div itemscope itemtype="http://schema.org/Person">
+
+  
+
+  <div class="author__content">
+    <h3 class="author__name" itemprop="name"></h3>
+    
+  </div>
+
+  <div class="author__urls-wrapper">
+    <button class="btn btn--inverse">Follow</button>
+    <ul class="author__urls social-icons">
+      
+
+      
+
+      
+
+      
+
+      
+
+      
+
+      
+
+      
+
+      
+
+      
+
+      
+
+      
+
+      
+        <li>
+          <a href="https://github.com/https://github.com/wildone" itemprop="sameAs">
+            <i class="fab fa-github" aria-hidden="true"></i> GitHub
+          </a>
+        </li>
+      
+
+      
+
+      
+
+      
+
+      
+
+      
+
+      
+
+      
+
+      
+
+      
+
+      
+
+      
+
+      
+        
+      
+      
+      
+
+      <!--
+  <li>
+    <a href="http://link-to-whatever-social-network.com/user/" itemprop="sameAs">
+      <i class="fa fa-fw" aria-hidden="true"></i> Custom Social Profile Link
+    </a>
+  </li>
+-->
+    </ul>
+  </div>
+</div>
+
+  
+  
+  </div>
+
+
+
+    <article class="page" itemscope itemtype="http://schema.org/CreativeWork">
+        
+        <meta itemprop="description" content="var idx = lunr(function () {  this.field(‘title’)  this.field(‘excerpt’)  this.field(‘categories’)  this.field(‘tags’)  this.ref(‘id’)">
+        
+        
+
+        <div class="page__inner-wrap">
+            
+            <header>
+
+
+                
+                
+            </header>
+            
+
+            <section class="page__content" itemprop="text">
+                var idx = lunr(function () {
   this.field('title')
   this.field('excerpt')
   this.field('categories')
@@ -67,3 +411,115 @@ $(document).ready(function() {
     }
   });
 });
+
+                
+            </section>
+
+            <style>
+                .page__content_adspace_bottom {
+                    height: 200px;
+                    display: inline-block;
+                    width: 100%;
+                }
+            </style>
+            <section class="page__content_adspace_bottom">
+            
+<div align="center" style="margin: 1em 0;">
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-6707033742178132"
+         data-ad-slot="7730324178"
+         data-ad-format="auto"
+         data-full-width-responsive="true"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+</div>
+
+            </section>
+
+            <footer class="page__meta">
+                
+                
+
+
+                
+            </footer>
+
+            
+
+            
+        </div>
+
+        
+        <div class="page__comments">
+  
+  
+      <h4 class="page__comments-title">Leave a Comment</h4>
+      <section id="disqus_thread"></section>
+    
+</div>
+
+        
+    </article>
+
+    
+    
+</div>
+
+    <div class="page__footer">
+      <footer>
+        <!-- start custom footer snippets -->
+
+<!-- end custom footer snippets -->
+        <div class="page__footer-follow">
+  <ul class="social-icons">
+    
+      <li><strong>Follow:</strong></li>
+    
+
+    
+
+    <li><a href="/feed.xml"><i class="fas fa-fw fa-rss-square" aria-hidden="true"></i> Feed</a></li>
+  </ul>
+</div>
+
+<div class="page__footer-copyright">&copy; 2020 Max Barrass. Powered by <a href="https://jekyllrb.com" rel="nofollow">Jekyll</a> &amp; <a href="https://mademistakes.com/work/minimal-mistakes-jekyll-theme/" rel="nofollow">Minimal Mistakes</a>.</div>
+
+      </footer>
+    </div>
+
+    
+  <script src="/assets/js/main.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/js/fontawesome.min.js"></script>
+
+
+
+
+
+
+
+    
+  <script>
+    var disqus_config = function () {
+      this.page.url = "https://maxbarrass.com/assets/js/lunr/lunr-en.js";  /* Replace PAGE_URL with your page's canonical URL variable */
+      this.page.identifier = ""; /* Replace PAGE_IDENTIFIER with your page's unique identifier variable */
+    };
+    (function() { /* DON'T EDIT BELOW THIS LINE */
+      var d = document, s = d.createElement('script');
+      s.src = 'https://maxbarrass.disqus.com/embed.js';
+      s.setAttribute('data-timestamp', +new Date());
+      (d.head || d.body).appendChild(s);
+    })();
+  </script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+
+
+  
+
+
+
+
+  </body>
+</html>
--- docker-serve.sh
+++ docker-serve.sh
@@ -3,4 +3,4 @@
 rvm install "ruby-2.6.3"
 gem install jekyll bundler jemoji nokogiri -n /usr/local/bin
 bundle install
-bundle exec jekyll serve --host 0.0.0.0 --livereload
+bundle exec jekyll serve --host 0.0.0.0 --livereload --force_polling
