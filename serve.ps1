4d72b9f7e72065c3ba92e94184fc8ded62176e98
Max Barrass <max.barrass@gmail.com>
2020-07-29 14:30:59 +1000

    update serve scripts [skip ci]

--- serve.ps1
+++ serve.ps1
@@ -1,3 +1,3 @@
 Write-Host "Starting Docker container to run build server"
 
-docker run -it --rm -p 4000:4000 -p 35729:35729 -v ${pwd}:/build/source:rw aemdesign/centos-java-buildpack bash --login /build/source/docker-serve.sh
+docker run -it --rm -p 4001:4000 -p 35730:35729 -v ${pwd}:/build/source:rw aemdesign/centos-java-buildpack bash --login /build/source/docker-serve.sh
