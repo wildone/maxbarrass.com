70129b7a2c36163bd63bfe1bfa3267a4a7b415c0
wildone <wildone@users.noreply.github.com>
2020-10-04 09:31:15 +0000

    Automated deployment: Sun Oct  4 09:31:15 UTC 2020 fb7dc81dd5bd79d1ac5c6fc8568fac08788e9160

--- serve.ps1
+++ serve.ps1
@@ -1,3 +1,14 @@
-Write-Host "Starting Docker container to run build server"
 
-docker run -it --rm -p 4001:4000 -p 35730:35729 -v ${pwd}:/build/source:rw aemdesign/centos-java-buildpack bash --login /build/source/docker-serve.sh
+$SITE_NAME = Get-Content .\CNAME -Raw
+$SITE_NAME = $SITE_NAME -replace "[.\n\r]", "-"
+$CONATINER_NAME = "build-$SITE_NAME"
+
+$DOCKER_INSPECT_CHECK=$( docker inspect --format "{{.State.Status}}" $CONATINER_NAME )
+
+if ($DOCKER_INSPECT_CHECK -contains "running") {
+  Write-Host "Shell into existing Docker container:"
+  docker exec -it $CONATINER_NAME bash
+} else {
+  Write-Host "Starting Docker container to run build server:"
+  docker run --name $CONATINER_NAME -it --rm -p 8102:8100 -p 35731:35729 -v ${PWD}:/build/source:rw aemdesign/centos-java-buildpack bash --login /build/source/docker-serve.sh
+}
