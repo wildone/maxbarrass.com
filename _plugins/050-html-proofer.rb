3638a7f9e8beb0f9d4506628fc9cfc8cf30195f2
wildone <max.barrass@gmail.com>
2024-04-22 20:24:34 +1000

    fix HTMLProofer config mames.

--- _plugins/050-html-proofer.rb
+++ _plugins/050-html-proofer.rb
@@ -5,25 +5,15 @@ require "html-proofer"
 Jekyll::Hooks.register :site, :post_write do |site|
   HTMLProofer.check_directory(site.config["destination"], opts = {
     :check_html => true,
-    :check_img_http => true,
     :disable_external => true,
     :report_invalid_tags => true,
     :assume_extension => ".html",
     :allow_hash_href => true,
     :verbose => true,
-    :internal_domains => ["localhost:8100","localhost:8111", "localhost"],
-    :file_ignore => ["/assets/", "/tools/"],
+    :swap_urls => { :%r{localhost:8100} => "https://maxbarrass.com", :%r{localhost:8111} => "https://maxbarrass.com", :%r{localhost} => "https://maxbarrass.com" },
+    :ignore_files => ["/assets/", "/tools/"],
     :root_dir => "_site",
-    :href_ignore => [
-      /.*#data=.*/,
-      /localhost:.*/,
-      /.*.localhost/,
-      /.*.localhost/,
-      /0.0.0.0:.*/,
-      /192.168/,
-      /\/\/.*/,
-    ],
-    :url_ignore => [
+    :ignore_urls => [
       /.*#data=.*/,
       /localhost:.*/,
       /.*.localhost/,
